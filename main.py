7ef55ef3b33f3a1d5cc59f5005cdc3fe11df514c
LyubovTatur <71625191+LyubovTatur@users.noreply.github.com>
2021-02-13 14:15:34 +0300

    miaw

--- main.py
+++ main.py
@@ -44,6 +44,25 @@ def encode(text, key_1,key_2):
                 if square_2[x][y] == letter_2:
                     x_2 = x
                     y_2 = y
+        if y_1 == y_2:
+            if x_1 != 5:
+                x_1+=1
+            else:
+                x_1=0
+            if x_2!=5:
+                x_2+=1
+            else:
+                x_2=0
+
+        if x_1 == x_2:
+            if y_1 != 5:
+                y_1 += 1
+            else:
+                y_1 = 0
+            if y_2 != 5:
+                y_2 += 1
+            else:
+                y_2 = 0
         #print(str(x_1)+' '+str(y_1) + ' ' +letter_1  + ' | ' + str(x_1) + ' '+ str(y_2)+ ' ' + square_2[x_1][y_2] )
         #print(str(x_2)+' '+str(y_2) + ' ' +letter_2  + ' | ' + str(x_2) + ' '+ str(y_1)+ ' ' + square_1[x_2][y_1] )
         #print('________')
@@ -54,8 +73,57 @@ def encode(text, key_1,key_2):
         #result = result+square_2[x_2][x_1]+square_1[y_1][y_2]
 
     return result
+
 def decode(text, key_1,key_2):
-    pass
+        range_val = len(text)
+        result = ''
+        square_1 = create_square(key_1)
+        square_2 = create_square(key_2)
+
+        if len(text) % 2 != 0:
+            range_val += 1
+            text += ' '
+        i = 0
+        while i < range_val:
+            letter_1 = text[i]
+            letter_2 = text[i + 1]
+            i += 2
+            x_1 = -1
+            y_1 = -1
+            x_2 = -1
+            y_2 = -1
+            for y in range(len(square_1)):
+                for x in range(len(square_1[0])):
+                    if square_1[x][y] == letter_1:
+                        x_1 = x
+                        y_1 = y\
+                    if square_2[x][y] == letter_2:
+                        x_2 = x
+                        y_2 = y
+            if y_1 == y_2:
+                x_1 -= 1
+                x_2 -= 1
+            if x_1 == x_2:
+                y_1 -= 1
+                y_2 -= 1
+
+
+            # print(str(x_1)+' '+str(y_1) + ' ' +letter_1  + ' | ' + str(x_1) + ' '+ str(y_2)+ ' ' + square_2[x_1][y_2] )
+            # print(str(x_2)+' '+str(y_2) + ' ' +letter_2  + ' | ' + str(x_2) + ' '+ str(y_1)+ ' ' + square_1[x_2][y_1] )
+            # print('________')
+            # result = result+square_2[y_1][y_2]+square_1[x_2][x_1]
+            # result = result+square_2[x_1][x_2]+square_1[y_2][y_1]
+            result = result + square_2[x_1][y_2] + square_1[x_2][y_1]
+            # result = result+square_2[y_2][y_1]+square_1[x_1][x_2]
+            # result = result+square_2[x_2][x_1]+square_1[y_1][y_2]
+            # ЗАДАНИЕ ОКР ПО ОКРУ ЗАДАНИЯ ЗАДАННОГО ОКРА РЯД СУММА РЯДА СЧИТАТЬ ФУПРШФПОФШРРПЖБИДВЗТТМРСНАЫЛМЛЛИЖ
+            # ПИСЬКА И ПОПКА СИСЬКА И ПИСЬКА
+            # ГОНЯХЧИК ПЛИЗКИ???
+            # НЯХОВОЕ (НЯШЕНОЕ(НЯШЕЧНОВОЕ)) ОК ПОГНАЛОВОЕ)
+            # КИНЬ ФОТКУ ЖОПЫ В ЮБКЕ ПЛИЗКИ??????????????????????
+            # ОК АРТЕМ ЛЮБИМЫЙ СДЕЛАЕМ ВСЕСТЕ ок
+
+        return result
 text = input('введите сообщение')
 key_1 = input('ведите первый ключ')
 key_2 = input('введите второй ключ')
@@ -63,4 +131,4 @@ print('зашифрованное')
 coded_text = encode(text, key_1,key_2)
 print(coded_text)
 print('расшифрованное')
-print(encode(coded_text, key_2,key_1))
+print(decode(coded_text, key_2,key_1))
